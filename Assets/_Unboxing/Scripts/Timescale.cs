ed0a51ae20dff9627117a71e8cfe3ec0086c88dd
ixean <69546115+ixean@users.noreply.github.com>
2020-08-14 22:07:51 -0700

    first post
    vrc sdk 3

---
+++ Assets/_Unboxing/Scripts/Timescale.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timescale : MonoBehaviour {
+    public float timespeed = 1f;
+	// Use this for initialization
+	void Start () {
+        Time.timeScale = timespeed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
